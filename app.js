1b2f4ad2d0b01f436cdc576852522f257959f152
dmync <yashodhaariyarathne587@gmail.com>
2023-07-02 22:22:35 +0530

    app

--- app.js
+++ app.js
@@ -4,8 +4,98 @@ const app = express();
 
 var server = http.createServer(app);
 
-app.get('/ver', (req, res) => {
-  res.status(200).send("ok")
+const io = require("socket.io")(server, 
+  {
+    allowEIO3: true, // false by default
+    cors: {
+      origin: "http://localhost:4200",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  });
+
+var path = require('path');
+
+app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+var connectedUsers = [];
+
+io.on('connection', (socket) => {
+
+  socket.on('send-img', (data) => {
+    
+    let recipientID = connectedUsers.filter(aUser => aUser.username === data.username).length ? connectedUsers.filter(aUser => aUser.username === data.username)[0].clientID : 0;
+    var d = new Date();
+    var datetime = d.toLocaleString();
+
+    console.log("img-rec", recipientID)
+    
+    if(recipientID)
+      socket.to(recipientID).emit('send-img', {
+        senderName: data.username,
+        msg: data.msg,
+        userID : socket.id,
+        datetime:datetime
+      });
+   
+  });
+
+ 
+  socket.on('joining msg', (username, callBack) => {
+
+
+    let idx = connectedUsers.findIndex(el => el.username === username);
+    console.log("idx----------------", idx)
+    if(idx < 0)
+      connectedUsers.push({username, clientID: socket.id});
+    else
+      connectedUsers[idx].clientID = socket.id;
+
+    console.log(connectedUsers)
+    console.log("in")
+    // send id back
+    callBack(socket.id); 
+
+    let recipientID = connectedUsers.filter(aUser => aUser.username === 'admin').length ? connectedUsers.filter(aUser => aUser.username === 'admin')[0].clientID : 0;
+    console.log("id============================", recipientID)
+
+    if(recipientID)
+      socket.to(recipientID).emit('connected-users', {
+        username,
+        userID : socket.id
+      });
+
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (data) => {
+
+    let recipientID = connectedUsers.filter(aUser => aUser.username === data.username).length ? connectedUsers.filter(aUser => aUser.username === data.username)[0].clientID : 0;
+    var d = new Date();
+    var datetime = d.toLocaleString();
+
+    if(recipientID)
+      socket.to(recipientID).emit('chat message', {
+        senderName: data.username,
+        msg: data.msg,
+        userID : socket.id,
+        datetime:datetime
+      });
+ 
+  });
+
+
+
+
 });
 
 
